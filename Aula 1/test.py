3ed0bd251cd972a04aca105f26666ed2ed2d36af
Renan Brito Azevedo <20261011110006@ifrn.local>
2026-03-19 10:22:59 -0300

    test.py

--- Aula 1/test.py
+++ Aula 1/test.py
@@ -1,1 +1,29 @@
-print('Japão é fera!')
+import subprocess
+
+class User:
+    def __init__(self):
+        self.__nome = ''
+    
+    def set_nome(self, nome):
+        self.__nome = nome
+
+    def __str__(self):
+        return self.__nome
+
+class Interface:
+    @staticmethod
+    def main():
+        subprocess.run(['cls'], shell = True)
+        me = User()
+
+        nome = input('Insira seu nome: ')
+        me.set_nome(nome)
+
+        print(f'\nSeja bem vindo, {me}!')
+
+Interface.main()
+
+
+
+
+        
