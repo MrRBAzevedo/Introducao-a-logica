7ba9071e9159fc2ba94e71ec9e8965a8433be663
Renan Azevedo <renan.azevedo@academico.ifrn.edu.br>
2026-03-28 12:32:21 -0300

    questao01

--- 28.03 - Sábado/lista/questao01.py
+++ 28.03 - Sábado/lista/questao01.py
@@ -1,1 +1,6 @@
+a = float(input('Insira um número: '))
+b = float(input('Insira outro número: '))
+c = float(input('Insira outro número: '))
 
+menor = min(a, b, c)
+print(f'O menor dos três números é: {menor}')
