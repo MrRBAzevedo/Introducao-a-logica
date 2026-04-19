10016acb5a82d01c046322bfeb39d226b7731d97
Renan <renan.azevedo@academico.ifrn.edu.br>
2026-04-19 12:13:41 -0300

    Teste

--- Pygame/aula1.py
+++ Pygame/aula1.py
@@ -9,7 +9,7 @@ from sys import exit
 pygame.init()
 
 # define a janela do jogo, uma variável com tamanho definido por uma tupla (x, y)
-largura_tela = 640
+largura_tela = 1000
 altura_tela = 480
 tela = pygame.display.set_mode((largura_tela, altura_tela))
 
