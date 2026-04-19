4d2f816f1eba4f5e6cac962ff5ebf335ac4d8f12
Renan <renan.azevedo@academico.ifrn.edu.br>
2026-04-19 11:54:40 -0300

    Teste 3

---
+++ Aula 5/Questões OBI/teste3.py
@@ -0,0 +1,19 @@
+marcus = False
+juca = False
+roberto = False
+maria = False
+
+marcus = True
+
+if marcus == True:
+    juca = True
+
+if juca == True:
+    roberto = True
+
+if roberto == True:
+    maria = True
+
+print(juca)
+print(roberto)
+print(maria)
