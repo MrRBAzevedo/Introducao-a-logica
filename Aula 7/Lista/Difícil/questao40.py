946b387feecf9eb13db3aa000fc11c49d1cd4a98
Renan <renan.azevedo@academico.ifrn.edu.br>
2026-05-02 10:14:32 -0300

    teste.py

--- Aula 7/Lista/Difícil/questao40.py
+++ Aula 7/Lista/Difícil/questao40.py
@@ -1,2 +1,27 @@
-# Bora lá!
+seq = []
+entrada = 0
 
+while entrada != -1:
+    entrada = int(input())
+    seq.append(entrada)
+
+tartaruga = seq[0]
+lebre = seq[seq[0]]
+
+while tartaruga != -1:
+    if lebre == tartaruga:
+        lebre = seq[tartaruga]
+        tamanho = 1
+        while lebre != tartaruga:
+            lebre = seq[lebre]
+            tamanho += 1
+
+        print('Há ciclo')
+        print(f'Tamanho do ciclo: {tamanho}')
+
+        break
+
+    tartaruga = seq[tartaruga]
+    lebre = seq[seq[lebre]]
+else:
+    print('Não há ciclo')
--- Aula 7/teste.py
+++ Aula 7/teste.py
@@ -1,20 +1,9 @@
-contador = int(input())
+x = int(input())
+y = int(input())
 
-binario = bin(contador)[2:]
-binario = list(str(binario))
-binario = [int(bi) for bi in binario]
-binario.insert(0, 0)
-binario.insert(1, 0)
-binario.insert(2, 0)
-binario.append(0)
+x_b = bin(x)[2:]
+y_b = bin(y)[2:]
 
-pares = 0
-cont = 3
-
-while cont < len(binario):
-    if binario[cont] == 0 and binario[cont - 1] == 1 and binario[cont - 2] == 1 and binario[cont - 3] == 0:
-        pares += 1
-    
-    cont += 1
-
-print(pares)
+print(x_b)
+print(y_b)
+print(x_b + y_b)
