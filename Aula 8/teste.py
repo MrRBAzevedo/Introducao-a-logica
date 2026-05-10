fa5095b0a4b302cf67324beebf30cf0797e6dd06
Renan <renan.azevedo@academico.ifrn.edu.br>
2026-05-09 21:21:07 -0300

    Teste

--- Aula 8/teste.py
+++ Aula 8/teste.py
@@ -1,4 +1,26 @@
-maior = 0
-numero = 0
+chave = 'termo'
+chave = list(chave)
 
-if numero > maior: maior = numero
+while True:
+    entrada = list(input().lower())
+
+    if len(entrada) != 5:
+        print('Insira somente palavras com 5 letras')
+        continue
+
+    if entrada == chave:
+        print('Você descobriu a palavra chave!')
+        break
+    else:
+        dica = ''
+
+        for i in range(5):
+            if entrada[i] == chave[i]:
+                dica += 'C'
+            elif entrada[i] in chave:
+                dica += 'L'
+            else:
+                dica += 'E'
+
+        print(dica)
+    
