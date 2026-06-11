0ea6bb0728355b85653fcf4fee99bf569c8c2abe
Renan Azevedo <renan.azevedo@academico.ifrn.edu.br>
2026-06-11 14:31:29 +0000

    OBi

--- OBI - 2021/Testes/teste.py
+++ OBI - 2021/Testes/teste.py
@@ -1,3 +1,10 @@
-lista = [1, 2, 3, 4, 5]
-lista_copia = lista[0:len(lista)]
-print(lista_copia)
+P = int(input())
+O = int(input())
+cafes = 0
+
+for p in range(1, P+1):
+    for o in range(1, O+1):
+        if o % 2 == 0 and p% 2 == 0:
+            if o / p ==  2:
+                cafes += 1
+print(cafes)
