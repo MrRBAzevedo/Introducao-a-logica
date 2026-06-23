9314937d76e29cc47aadb3e3bbf1b0ca040f7ee9
Renan <renan.azevedo@academico.ifrn.edu.br>
2026-06-23 13:44:57 -0300

    Conferir

--- Aula 10/Lista/Médio/questao01.py
+++ Aula 10/Lista/Médio/questao01.py
@@ -1,3 +1,17 @@
 n = int(input())
 lista = list(map(int, input().split()))
+lista.sort()
+contador = 1
+maior = 0
 
+for i in range(1, n):
+    if lista[i] == lista[i - 1]:
+        contador += 1
+    else:
+        contador = 1
+
+    if contador > maior:
+        maior = contador
+        resultado = lista[i]
+
+print(resultado)
