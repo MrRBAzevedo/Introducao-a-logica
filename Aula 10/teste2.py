e7e556bfdc0fc632e51a9f43093662345a915f4f
Renan <renan.azevedo@academico.ifrn.edu.br>
2026-07-12 18:44:11 -0300

    Testes, testes e mais testes

--- Aula 10/teste2.py
+++ Aula 10/teste2.py
@@ -1,2 +1,15 @@
-lista = [1, 2, 3, 4, 5]
-print(sum(lista[0:0]))
+lista = [1, 2, 1, 1, 2]
+pont_esq = 0
+pont_dir = 1
+soma_atual = lista[0]
+soma = 3
+
+if soma_atual < soma:
+    soma_atual += lista[pont_dir]
+    pont_dir += 1
+
+
+
+
+
+print(lista[pont_esq:pont_dir])
